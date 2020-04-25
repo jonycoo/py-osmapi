c52d83afe7e92ba0f6a31ce8fd997ede55a90a81
Jonathan Koehler <jokehler.7@protonmail.com>
2020-04-25 12:52:14 +0200

    using issue class for future expansion. generator to limit RAM usage.

--- osmose.py
+++ osmose.py
@@ -4,66 +4,61 @@ Documentation, License etc.
 @package osmote
 '''
 
+import logging
 import math
 import requests
 import json
 
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+logger = logging.getLogger(__name__)
 
-url = 'http://osmose.openstreetmap.fr/en/api/0.2/errors'
-
+URL = 'http://osmose.openstreetmap.fr/en/api/0.2/errors'
 lang = 'en'
 user = 'jonycoo'
 
 
+class Issue:
+    def __init__(self, lat, lon, e_id, title, subtitle, elems):
+        self.lat = lat
+        self.lon = lon
+        self.id = e_id
+        self.title = title
+        self.subtitle = subtitle
+        self.elems = elems
+
+    @property
+    def loc(self):
+        return self.lat, self.lon
+
+    @classmethod
+    def from_api_issue(cls, lst):
+        elems = str(lst[6]).split('_')
+        return cls(lst[0], lst[1], lst[2], lst[9], lst[8], elems)
+
+    @classmethod
+    def gen_issue(cls, issue_lst):
+        for issue in issue_lst:
+            yield cls.from_api_issue(issue)
+
+    def __str__(self):
+        return '"{}" Issue at: {}, elems: {}'.format(self.title, self.loc, self.elems)
+
+
 def get_issues_user(user):
-    return requests.get(url +'?full=true&username=' + user).json()
-
-
-# def create_bbox(lat, lon, radius):
-#     R = float(6371.000785) # R is radius of earth in km
-#     x = lon
-#     y = lat
-#     dy = 360 * radius / R
-#     dx = dy * math.cos(math.radians(y))
-#     se = x+dx, y+dy
-#     nw = x-dx, y-dy
-#     return se, nw
+    return requests.get(URL + '?full=true&username=' + user).json()
 
 
 def get_issues_loc(lat, lon):
     path = '?full=true&lat={}&lon={}'
     path = path.format(lat, lon)
-    print(path)
-    isDic = requests.get(url + path).json()
-    return isDic
-
-
-def noteIssue(issue):
-    a = 5
+    return requests.get(URL + path).json()
 
 
 issues = get_issues_user(user)
-print(issues)
+for issue in Issue.gen_issue(issues['errors']):
+    print(issue)
+
+issues = get_issues_loc(49.14161, 9.22224)
 for issue in issues['errors']:
     print(issue[2])
 
-issuesl = get_issues_loc(49.14161, 9.22224)
-for issue in issuesl['errors']:
-    print(issue[2])
-
-'''
-    0  : "lat",
-    1  : "lon",
-    2  : "error_id",
-    3  : "item",
-    4  : "source",
-    5  : "class",
-    6  : "elems",
-    7  : "subclass",
-    8  : "subtitle",
-    9  : "title",
-    10 : "level",
-    11 : "update",
-    12 : "username"
-
-'''
